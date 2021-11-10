86afc805e42f9ba964f408e4cebc0d366ff14aba
luccasrojas <62764177+luccasrojas@users.noreply.github.com>
2021-11-10 14:59:30 -0500

    update

--- src/Prim.java
+++ src/Prim.java
@@ -3,6 +3,8 @@ public class Prim
 {
     public static void main(String[]args) 
     {
+        Grafo grafoEjemplo = new Grafo(0,null);
+
     }
 
     /*static class Arco
@@ -35,10 +37,11 @@ public class Prim
         public void agregarNodo(int valor, int peso, int nodoRaiz)
         {
             this.listaAdyacencia[nodoRaiz].add(new Node(valor,peso));
+            this.vertices= this.vertices+1;
         }
     }
 
-    
+
     public class Node
     {
         int value;
@@ -55,7 +58,28 @@ public class Prim
     {
         LinkedList<Node> result = new LinkedList<Node>();
         result.add(grafo.listaAdyacencia[nodoInicial].getFirst());
+        ArrayList<Integer> recorridos = new ArrayList<Integer>();
+        while (result.size() < grafo.vertices)
+        {
+            Integer infinito = Integer.MAX_VALUE;
+            int min=infinito;
+            Node minNode = new Node(-1,infinito);
+            for (Node nodo: result)
+            {
+                LinkedList<Node> listaNodo = grafo.listaAdyacencia[nodo.value];
+                for (Node adyacente:listaNodo)
+                {
+                    if (adyacente.peso<min && !recorridos.contains(adyacente.value))
+                    {
 
+                        min = adyacente.peso;
+                        minNode = adyacente;
+                    }
+                }
+            }
+            recorridos.add(minNode.value);
+            result.add(minNode);
+        }
         return result;
     }
 }
