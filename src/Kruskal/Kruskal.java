f9233091891aacb3135f748f230b17b42d1fc74c
luccasrojas <l.rojasb@uniandes.edu.co>
2021-11-15 14:27:24 -0500

    Kruscal opcion 1

--- src/Kruskal/Kruskal.java
+++ src/Kruskal/Kruskal.java
@@ -6,7 +6,7 @@ public class Kruskal
 {
     public static void main(String[] args)
     {
-        Grafo grafoEjemplo = new Grafo(5);
+        Grafo grafoEjemplo = new Grafo(6);
         grafoEjemplo.agregarArco(0,1,6);
         grafoEjemplo.agregarArco(0,2,1);
         grafoEjemplo.agregarArco(0,3,5);
@@ -17,8 +17,11 @@ public class Kruskal
         grafoEjemplo.agregarArco(2,4,6);
         grafoEjemplo.agregarArco(3,5,2);
         grafoEjemplo.agregarArco(4,5,6);
-        mergesort(grafoEjemplo.listaArcos,0,grafoEjemplo.listaArcos.size());
-        
+        mergesort(grafoEjemplo.listaArcos,0,grafoEjemplo.listaArcos.size()-1);
+        for(Arco a:grafoEjemplo.listaArcos)
+        {
+            System.out.println(a.peso);
+        }       
     }
     
     static class Arco
@@ -55,11 +58,18 @@ public class Kruskal
 
     public static void merge(ArrayList<Arco> arcos,int izq, int m, int der){
         int i, j, k;
-        ArrayList<Arco> aux = arcos; //array auxiliar
-        //for (i=izq; i<=der; i++)      copia ambas mitades en el array auxiliar                                       
-             //B[i]=A[i];
-             ///arcos.remove(i);
-             ///arcos.add(i,new Arco(1,2,3));
+        ArrayList<Arco> aux = new ArrayList<Arco>(); //array auxiliar
+        for(i=0; i<=arcos.size(); i++) 
+        {
+            aux.add(new Arco(0,0,0));
+        }
+        for (i=izq; i<=der; i++)      //copia ambas mitades en el array auxiliar                                       
+        //B[i]=A[i];
+        {
+            aux.set(i,arcos.get(i));
+        }
+        //arcos.remove(i);
+        //arcos.add(i,new Arco(1,2,3));
 
 
         i=izq; j=m+1; k=izq;
@@ -68,21 +78,18 @@ public class Kruskal
                //A[k++]=B[i++];
                if (aux.get(i).peso<=aux.get(j).peso)
                {
-                   arcos.remove(k++);
-                   arcos.add(k++,aux.get(i++));
+                   arcos.set(k++,aux.get(i++));
                }
                //else
                    //A[k++]=B[j++];
                 else
                 {
-                    arcos.remove(k++);
-                    arcos.add(k++,aux.get(j++));
+                    arcos.set(k++,aux.get(j++));
                 }
              
         while (i<=m)         //copia los elementos que quedan de la
         {
-            arcos.remove(k++);
-            arcos.add(k++,aux.get(i++));
+            arcos.set(k++,aux.get(i++));
         }
               //A[k++]=B[i++]; //primera mitad (si los hay)
      }
@@ -113,10 +120,9 @@ public class Kruskal
             this.listaArcos.add(new Arco(nodoInicio, nodoFin, peso));
         }
         
-        public ArrayList<Arco> ordenarArcos()
+        public void ordenarArcos()
         {
-            ArrayList<Arco> resultado = mergesort(listaArcos,0);
-            return resultado;
+            mergesort(listaArcos,0,listaArcos.size());
         }
     }
 
@@ -124,10 +130,10 @@ public class Kruskal
     /** 
      * @param grafo
      */
-    public void kruskal(Grafo grafo)
+    public Grafo kruskal(Grafo grafo)
     {
+        Grafo result = new Grafo(grafo.vertices);
         int[][] matriz = grafo.matAdj;
-        ArrayList<Integer> tamano = grafo.listaDeNodos;
         ArrayList<Integer> visitados = new ArrayList<Integer>();
         for (int i = 0; i < matriz.length; i++)
         {
@@ -140,5 +146,6 @@ public class Kruskal
             }
             visitados.add(i);
         }
+        return result;
     }
 }
